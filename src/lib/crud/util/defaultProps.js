ea2949af25722386abf094975a8b6f918f2f0d14
Jose A Martinez <jamroa67@gmail.com>
2019-10-22 10:32:34 -0400

    Fixed bug with fieldTypes

--- src/lib/crud/util/defaultProps.js
+++ src/lib/crud/util/defaultProps.js
@@ -1,98 +1,99 @@
 
 
 /**Completes with default values the missing configuration attributes provided by the user.*/
-export const getConfig = (customConfig) => { 
+export const getConfig = (customConfig) => {
     let fieldsT = getFieldTypes(customConfig.fieldTypes, customConfig.fields)
-    customConfig.fields = customConfig.fields.map(e=>e.toLowerCase())
-    customConfig.fieldsToDisplay = customConfig.fieldsToDisplay ? customConfig.fieldsToDisplay.map(e=>e.toLowerCase()): []
+    customConfig.fields = customConfig.fields.map(e => e.toLowerCase())
+    customConfig.fieldsToDisplay = customConfig.fieldsToDisplay ? customConfig.fieldsToDisplay.map(e => e.toLowerCase()) : []
     let dn = getDisplayNames(customConfig, fieldsT)
-    let defaultConfigProps = {                  
-            fieldsToDisplay: [],
-            fieldsToHide: [],
-            fieldsToDisplayNames:dn,       
-            labelsConfig:{
-                caseType: 'capital'
+    let defaultConfigProps = {
+        fieldsToDisplay: [],
+        fieldsToHide: [],
+        fieldsToDisplayNames: dn,
+        labelsConfig: {
+            caseType: 'capital'
+        },
+        fieldTypes: fieldsT,
+        tableConfig: {
+            className: 'table table-striped table-bordered',
+            deleteActionClassName: 'btn btn-danger',
+            editActionClassName: "btn btn-warning",
+            viewActionClassName: 'btn btn-info',
+            deleteActionCallBack: null,
+            editActionCallBack: null,
+            viewActionCallBack: null,
+        },
+        labels: {
+            listView: {
+                searchBoxPlaceholder: "Search {entity}",
+                showRowsPerPages: "Show {select} {entity} per page",
+                showingRows: "Showing from {start} to {end} of {rows} {entity} (Page {page} of {pages})",
+                noRows: "No {entity} found",
+                actionsHeader: "Actions",
+                deleteAction: "delete",
+                editAction: "edit",
+                viewAction: "view",
+                deleteConfirmMessage: 'Are you sure?'
             },
-            fieldTypes:fieldsT,
-            tableConfig:{
-                className: 'table table-striped table-bordered',
-                deleteActionClassName: 'btn btn-danger',
-                editActionClassName: "btn btn-warning",
-                viewActionClassName: 'btn btn-info',
-                deleteActionCallBack: null,
-                editActionCallBack: null,
-                viewActionCallBack: null,            
-            },
-            labels:{
-                listView:{
-                    searchBoxPlaceholder: "Search {entity}",
-                    showRowsPerPages: "Show {select} {entity} per page",
-                    showingRows: "Showing from {start} to {end} of {rows} {entity} (Page {page} of {pages})",
-                    noRows: "No {entity} found",
-                    actionsHeader: "Actions",
-                    deleteAction:"delete",
-                    editAction:"edit",
-                    viewAction:"view",
-                    deleteConfirmMessage:'Are you sure?'
-                },
-                editView:{
-                    saveButton:'save',
-                    backButton:'back',
-                }
-            },
-            actions:['edit', 'view', 'delete', 'create'],
-            actionsCallbacks:{
-                edit:null,
-                delete:null,
-                create:null
+            editView: {
+                saveButton: 'save',
+                backButton: 'back',
             }
+        },
+        actions: ['edit', 'view', 'delete', 'create'],
+        actionsCallbacks: {
+            edit: null,
+            delete: null,
+            create: null
         }
-    
+    }
+
 
     return {
-        ...defaultConfigProps, 
+        ...defaultConfigProps,
         ...customConfig,
         labels: {
             ...defaultConfigProps.labels,
             ...customConfig.labels,
-            listView:{
-            ...defaultConfigProps.labels.listView,                
-            ...customConfig.labels.listView
+            listView: {
+                ...defaultConfigProps.labels.listView,
+                ...customConfig.labels.listView
             }
         },
-        fieldTypes:{
+        fieldTypes: [
             ...defaultConfigProps.fieldTypes,
-            ...customConfig.fieldTypes,            
-        }
+            ...customConfig.fieldTypes,
+
+        ]
     }
 
 }
 let getFieldTypes = (fieldTypes, fields) => {
-    return fields.map(e=>{
-        if(fieldTypes && fieldTypes.length > 0){
-            for(let i=0;i<fieldTypes.length;i++){
-                if(fieldTypes[i].fieldName === e)
-                    return {...fieldTypes[i]}                 
+    return fields.map(e => {
+        if (fieldTypes && fieldTypes.length > 0) {
+            for (let i = 0; i < fieldTypes.length; i++) {
+                if (fieldTypes[i].fieldName === e)
+                    return { ...fieldTypes[i] }
             }
         }
         return {
-            fieldName:e,
-            fieldDisplayName:e,
-            type:null
+            fieldName: e,
+            fieldDisplayName: e,
+            type: null
         }
     })
 }
 
 let getDisplayNames = (config, fieldTypes) => {
-    let headerNames = []               
-    if(config.fieldsToDisplay && config.fieldsToDisplay.length > 0)
+    let headerNames = []
+    if (config.fieldsToDisplay && config.fieldsToDisplay.length > 0)
         headerNames = config.fieldsToDisplay
-    else if(config.fieldsToHide && config.fieldsToHide.length > 0){
-        headerNames = config.fields.filter(e=> !config.fieldsToHide.map(e=>e.toLowerCase()).includes(e) )
-     } else {
+    else if (config.fieldsToHide && config.fieldsToHide.length > 0) {
+        headerNames = config.fields.filter(e => !config.fieldsToHide.map(e => e.toLowerCase()).includes(e))
+    } else {
         headerNames = config.fields
-    }                                
-    return fieldTypes.filter(f=> headerNames.includes(f.fieldName.toLowerCase())).map(e=>e.fieldDisplayName)   
+    }
+    return fieldTypes.filter(f => headerNames.includes(f.fieldName.toLowerCase())).map(e => e.fieldDisplayName)
 }
 
 /**Completes and auto-generate some data values that will be used througout the screens. */
@@ -103,7 +104,7 @@ export const getData = (data) => {
 }
 /**Gets the first level fields names based in the entities data provided. */
 let getFields = (entities) => {
-    if(entities.length <= 0) return []
+    if (entities.length <= 0) return []
     let firstLevel = Object.keys(entities[0])
     return firstLevel
 }
